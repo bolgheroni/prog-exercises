a137d253cbb7b701d913803d2e27ef0e9f300e5d
bolgheroni <robertobolgheroni@gmail.com>
2024-01-13 17:52:43 -0300

    First commit

---
+++ Leetcode/Básicos/valid_parentheses.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<string> pars = new Stack<string>();
+
+        for (int i = 0; i< s.Length; i++ ) {
+            char currentPar = s[i];
+
+            if ()
+        }
+    }
+}
