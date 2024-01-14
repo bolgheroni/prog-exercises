563834aecef1c0de6d2f0f2e5c51d4f629873804
bolgheroni <robertobolgheroni@gmail.com>
2024-01-13 22:31:52 -0300

    Leetcode basics: merge sorted lists

---
+++ Leetcode/Basics/merge_sorted_lists.cs
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+
+    private ListNode GetLesserNode(ListNode a, ListNode b) {
+        if (a == null) {
+            return b;
+        }
+        if (b == null) {
+            return a;
+        }
+
+        if (a.val < b.val) {
+            return a;
+        }
+        return b;
+    }
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode mergeFront = new ListNode(-1, null);
+        ListNode currentMergeTail = mergeFront;
+
+        ListNode current1Tail = list1;
+        ListNode current2Tail = list2;
+
+        while (current1Tail != null || current2Tail != null) {
+            ListNode lesser = GetLesserNode(current1Tail, current2Tail);
+            
+            if (lesser == current1Tail){
+                current1Tail = current1Tail.next;
+            } else {
+                current2Tail = current2Tail.next;
+            }
+            
+            currentMergeTail.next = new ListNode(lesser.val, null);
+            currentMergeTail = currentMergeTail.next;            
+        }
+
+        return mergeFront.next;   
+
+    }
+}
