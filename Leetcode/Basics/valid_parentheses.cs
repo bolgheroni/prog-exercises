6e312cc3c78ba4283bfc7779c331616bb58ad7ba
bolgheroni <robertobolgheroni@gmail.com>
2024-01-13 22:09:48 -0300

    Leetcode basics: valid parentheses

--- Leetcode/Basics/valid_parentheses.cs
+++ Leetcode/Basics/valid_parentheses.cs
@@ -1,11 +1,51 @@
+using System;
+using System.Collections.Generic;
+
 public class Solution {
+    
+    public bool isWellFormed(char parOpen, char parClose) {
+        if (parOpen == '(' && parClose == ')') {
+            return true;
+        }
+        if (parOpen == '{' && parClose == '}') {
+            return true;
+        }
+        if (parOpen == '[' && parClose == ']') {
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool isOpenPar(char c) {
+        if (c == '(' || c == '{' || c == '['){
+            return true;
+        }
+        return false;
+    }
+
     public bool IsValid(string s) {
-        Stack<string> pars = new Stack<string>();
+        Stack<char> pars = new Stack<char>();
 
         for (int i = 0; i< s.Length; i++ ) {
             char currentPar = s[i];
+            
+            if (isOpenPar(currentPar)) {
+                pars.Push(currentPar);
+            } else {
+                if (pars.Count == 0) {
+                    return false;
+                }
+                char topChar = pars.Pop();
+                if (!isWellFormed(topChar, currentPar)) {
+                    return false;
+                }
+            }
 
-            if ()
         }
+        if (pars.Count > 0) {
+            return false;
+        }
+        return true;
     }
 }
