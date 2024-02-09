fc0c8f2ba9dd122f37b5037d783b99e0d4c72eb5
Roberto Bolgheroni <roberto.bolgheroni@ada.tech>
2024-02-09 08:14:45 -0300

    LC Basics: LinkedListCycle top 8%-speed (13 min)

--- Leetcode/Basics/LinkedListCycleEx.cs
+++ Leetcode/Basics/LinkedListCycleEx.cs
@@ -18,14 +18,21 @@ public class LinkedListCycleEx {
         }
 
         var current = head;
-
+        // the idea is to point all nexts to the head
+        // if at any point we get a node that points to the head, either it actually points to the head (forms a loop),
+        // or we are in a node that we already visited (traversed a loop)
         while (current.next != null) {
             if (current.next.Equals(head)) {
                 return true;
             }
-            var oldCurrent = current;
-            current = oldCurrent.next;
-            oldCurrent.next = head;
+            // all of this logic is just me saving up an aux variable to perform the exchange
+            head.next = current.next;
+            if (!current.Equals(head)){
+                // handling when we're in the head
+                current.next = head;    
+            }
+            current = head.next;
+            head.next = head;
         }
 
         return false;
