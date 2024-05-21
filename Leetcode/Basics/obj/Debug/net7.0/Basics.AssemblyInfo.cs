05bacca00b93a6cad744acde04c26b4f81e2114e
Roberto Bolgheroni <roberto.bolgheroni@ada.tech>
2024-05-21 18:22:21 -0300

    Add files?
    Signed-off-by: Roberto Bolgheroni <roberto.bolgheroni@ada.tech>

--- Leetcode/Basics/obj/Debug/net7.0/Basics.AssemblyInfo.cs
+++ Leetcode/Basics/obj/Debug/net7.0/Basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca6f5d8e90fed9d73b03434345d7035297daf6fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
