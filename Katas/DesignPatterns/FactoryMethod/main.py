a5e3611af6c1bd82080783ade504fd37868d5833
Roberto Bolgheroni <roberto.bolgheroni@ada.tech>
2024-05-25 19:39:54 -0300

    FactoryMethod: main is refactored
    Signed-off-by: Roberto Bolgheroni <roberto.bolgheroni@ada.tech>

--- Katas/DesignPatterns/FactoryMethod/main.py
+++ Katas/DesignPatterns/FactoryMethod/main.py
@@ -9,16 +9,11 @@ def main():
     transports_list: List[Transport] = []
     print("Factory Method Pattern \n")
     print("----------------------\n")
+    
     while action != "4":
-        print("\n")
-        print("----------------------\n")
-        print("Actions: \n")
-        print("1. List ongoing transports")
-        print("2. Add new transport")
-        print("3. Deploy transport")
-        print("4. Exit")
-        print("----------------------\n")
-        action = input("Choose an action: ")
+        print("\n----------------------\n")
+        
+        action = get_action_input()
 
         if action == "1":
             list_transports(transports_list)
@@ -29,7 +24,14 @@ def main():
         if action == "3":
             deploy_transport(transports_list)
         
-
+def get_action_input() -> str:
+    print("Actions: \n")
+    print("1. List ongoing transports")
+    print("2. Add new transport")
+    print("3. Deploy transport")
+    print("4. Exit")
+    print("----------------------\n")
+    return input("Choose an action: ")
 
 
 if __name__ == "__main__":
