bbb1023c77cd46f9ab76b49a614e59eee74eb00b
Roberto Bolgheroni <roberto.bolgheroni@ada.tech>
2024-05-25 19:58:13 -0300

    FactoryMethod: main is refactored
    Signed-off-by: Roberto Bolgheroni <roberto.bolgheroni@ada.tech>

--- Katas/DesignPatterns/FactoryMethod/main.py
+++ Katas/DesignPatterns/FactoryMethod/main.py
@@ -3,34 +3,40 @@ from add_transport import add_transport
 from list_transports import list_transports
 from deploy_transport import deploy_transport
 from typing import List
+import constants
 
 def main():
     action = ""
     transports_list: List[Transport] = []
     print("Factory Method Pattern \n")
-    print("----------------------\n")
-    
-    while action != "4":
-        print("\n----------------------\n")
+
+    while action != constants.EXIT_ACTION:
+        print("----------------------")
         
         action = get_action_input()
 
-        if action == "1":
+        if action == constants.LIST_TRANSPORTS_ACTION:
             list_transports(transports_list)
         
-        if action == "2":
+        if action == constants.ADD_TRANSPORT_ACTION:
             add_transport(transports_list)
         
-        if action == "3":
+        if action == constants.DEPLOY_TRANSPORT_ACTION:
             deploy_transport(transports_list)
         
 def get_action_input() -> str:
-    print("Actions: \n")
-    print("1. List ongoing transports")
-    print("2. Add new transport")
-    print("3. Deploy transport")
-    print("4. Exit")
-    print("----------------------\n")
+    actions = {
+        constants.LIST_TRANSPORTS_ACTION: "List transports",
+        constants.ADD_TRANSPORT_ACTION: "Add transport",
+        constants.DEPLOY_TRANSPORT_ACTION: "Deploy transport",
+        constants.EXIT_ACTION: "Exit"
+    }
+    print("Actions:")
+    sorted_actions = sorted(actions.items())
+    for action in sorted_actions:
+        print(f"{action[0]}. {action[1]}")
+    
+    print("----------------------")
     return input("Choose an action: ")
 
 
