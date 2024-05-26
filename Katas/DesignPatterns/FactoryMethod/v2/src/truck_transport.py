9c3e470405ef572841262b0bfb0a8f9ad59eff13
Roberto Bolgheroni <roberto.bolgheroni@ada.tech>
2024-05-25 22:12:11 -0300

    FactoryMethod(v2): truck_transport eta method is implemented
    Signed-off-by: Roberto Bolgheroni <roberto.bolgheroni@ada.tech>

--- Katas/DesignPatterns/FactoryMethod/v2/src/truck_transport.py
+++ Katas/DesignPatterns/FactoryMethod/v2/src/truck_transport.py
@@ -1,13 +1,19 @@
 from v2.src.transport import Transport
 from v2.src.eta import ETA
+import math
 
 class TruckTransport(Transport):
+    _SPEED_KM_H = 50.0
     def __init__(self, id: int, distance_km: int):
         super().__init__(type="TRUCK", id=id)
         self._distance_km = distance_km
     
-    def eta(self):
-        return ETA(hours=2, minutes=12)
+    def eta(self):        
+        travel_time_mins = 60 * self._distance_km / self._SPEED_KM_H
+        return ETA(
+            hours=math.floor(travel_time_mins // 60),
+            minutes=math.floor(travel_time_mins % 60)
+        )
 
     def __str__(self):
         base_description = super().__str__()
