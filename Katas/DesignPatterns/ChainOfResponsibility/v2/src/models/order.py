41b10891db34d988c4cc54be05a2b3d275fd08b1
Roberto Bolgheroni <roberto.bolgheroni@ada.tech>
2024-05-30 23:44:56 -0300

    CoResp(v2): refactor: order params are typed
    Signed-off-by: Roberto Bolgheroni <roberto.bolgheroni@ada.tech>

--- Katas/DesignPatterns/ChainOfResponsibility/v2/src/models/order.py
+++ Katas/DesignPatterns/ChainOfResponsibility/v2/src/models/order.py
@@ -1,6 +1,11 @@
+from src.models.order_item import OrderItem
+from typing import List
+
+
 class Order:
-    def __init__(self, items=[]):
-        self.items = items
+    def __init__(self, items: List[OrderItem] | None = None):
+        _items = items if items else list()
+        self.items = _items
 
     def is_empty(self):
         return not self.items
