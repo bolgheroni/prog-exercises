fbe80d441201e0b3de6b928c52eb480b0092f349
Roberto Bolgheroni <roberto.bolgheroni@ada.tech>
2024-05-30 23:41:38 -0300

    CoResp(v2): refactor: validation_handler methods are extracted
    Signed-off-by: Roberto Bolgheroni <roberto.bolgheroni@ada.tech>

--- Katas/DesignPatterns/ChainOfResponsibility/v2/src/handlers/validation_handler.py
+++ Katas/DesignPatterns/ChainOfResponsibility/v2/src/handlers/validation_handler.py
@@ -3,9 +3,23 @@ from src.models.handle_result import HandleResult
 from src.models.order import Order
 class ValidationHandler(Handler):
     def handle(self, order: Order) -> HandleResult:
+        empty_order_result = self._check_order_is_empty(order)
+        if empty_order_result:
+            return empty_order_result
+        
+        invalid_quantities_result = self._check_order_items_quantities(order)
+        if invalid_quantities_result:
+            return invalid_quantities_result
+        
+        return HandleResult(is_valid=True)
+    
+    def _check_order_is_empty(self, order: Order) -> HandleResult | None:
         if order.is_empty():
             return HandleResult(is_valid=False, cause="Empty order")
+        return None
+    
+    def _check_order_items_quantities(self, order: Order) -> HandleResult | None:
         for item in order.items:
             if item.quantity <= 0:
                 return HandleResult(is_valid=False, cause=f"Invalid quantity ({item.quantity}) for {item.product}")
-        return HandleResult(is_valid=True)
+        return None
