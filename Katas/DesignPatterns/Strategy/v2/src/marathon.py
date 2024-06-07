dc30755b7d892ddca3067ef830861f1d9aedd374
Roberto Bolgheroni <roberto.bolgheroni@ada.tech>
2024-06-07 08:53:34 -0300

    Str(v2): refactor: participant is added
    Signed-off-by: Roberto Bolgheroni <roberto.bolgheroni@ada.tech>

--- Katas/DesignPatterns/Strategy/v2/src/marathon.py
+++ Katas/DesignPatterns/Strategy/v2/src/marathon.py
@@ -1,9 +1,12 @@
+from typing import List
+from src.participant import Participant
+
 class Marathon:
-    def __init__(self):
-        self.participants = []
+    def __init__(self, participants: List[Participant]| None = None):
+        self.participants = participants or list()
 
     def add_participant(self, name, age):
         self.participants.append((name, age))
 
     def current_state(self):
-        return '\n'.join([f'{name} {age}' for name, age in self.participants])
+        return '\n'.join([f'{participant}' for participant in self.participants])
--- Katas/DesignPatterns/Strategy/v2/tests/test_marathon.py
+++ Katas/DesignPatterns/Strategy/v2/tests/test_marathon.py
@@ -1,13 +1,25 @@
 from src.marathon import Marathon
 
-def make_sut():
-    return Marathon()
+def make_sut(
+    participants=None
+):
+    _participants = participants or list([
+        ('John Doe', 30),
+        ('Jane Doe', 25)
+    ])
+    return Marathon(
+        participants=_participants
+    )
 
 def test_marathon_current_state():
     def test_displays_participants_data():
-        sut = make_sut()
-        sut.add_participant('John Doe', 30)
-        sut.add_participant('Jane Doe', 25)
+        participants = [
+            ('John Doe', 30),
+            ('Jane Doe', 25)
+        ]
+        sut = make_sut(
+            participants=participants
+        )
         current_state = sut.current_state()
 
         assert 'John Doe' in current_state
