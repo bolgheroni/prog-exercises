26c10f0a1973c7342eeb5dd6c37d826b81fa8dfc
Roberto Bolgheroni <roberto.bolgheroni@patagon.work>
2024-12-11 09:55:22 -0300

    AOC24: 11: add input multiple

--- AdventOfCode2024/11/test_expand_input_times.py
+++ AdventOfCode2024/11/test_expand_input_times.py
@@ -2,7 +2,7 @@ from models import Stone
 from solution import expand_input_times
 
 
-def test_expand_once_input():
+def test_expand_input_once():
     input_list = [
         Stone(125),
         Stone(17),
@@ -11,3 +11,59 @@ def test_expand_once_input():
     result = expand_input_times(input_list, 1)
 
     assert result == [Stone(253000), Stone(1), Stone(7)]
+
+
+def test_expand_input_times_2():
+    input_list = [
+        Stone(125),
+        Stone(17),
+    ]
+
+    result = expand_input_times(input_list, 2)
+
+    assert result == [253, 0, 2024, 14168]
+
+
+def test_expand_input_times_6():
+    input_list = [
+        Stone(125),
+        Stone(17),
+    ]
+
+    result = expand_input_times(input_list, 6)
+
+    assert result == [
+        2097446912,
+        14168,
+        4048,
+        2,
+        0,
+        2,
+        4,
+        40,
+        48,
+        2024,
+        40,
+        48,
+        80,
+        96,
+        2,
+        8,
+        6,
+        7,
+        6,
+        0,
+        3,
+        2,
+    ]
+
+
+def test_expand_input_times_25():
+    input_list = [
+        Stone(125),
+        Stone(17),
+    ]
+
+    result = expand_input_times(input_list, 25)
+
+    assert len(result) == 55312
