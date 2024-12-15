148a4f0114b2b22e2db29da9ee8cb43cbc059f7b
Roberto Bolgheroni <roberto.bolgheroni@patagon.work>
2024-12-15 13:03:08 -0300

    AOC24: 15: rows gm multiple rows tests

--- AdventOfCode2024/15/test_rows_game_map.py
+++ AdventOfCode2024/15/test_rows_game_map.py
@@ -26,3 +26,47 @@ def test_single_get_robot_position():
     robot_pos = gm.get_robot_position()
 
     assert robot_pos == Position(0, 0)
+
+
+def test_multiple_check_position():
+    gm = RowsGameMap(
+        [
+            [ElementType.WALL, ElementType.EMPTY, ElementType.WALL],
+            [ElementType.WALL, ElementType.EMPTY, ElementType.WALL],
+            [ElementType.WALL, ElementType.ROBOT, ElementType.WALL],
+        ]
+    )
+
+    pos = gm.check_position(Position(2, 1))
+
+    assert pos == ElementType.ROBOT
+
+
+def test_multiple_set_element():
+    gm = RowsGameMap(
+        [
+            [ElementType.WALL, ElementType.EMPTY, ElementType.WALL],
+            [ElementType.WALL, ElementType.EMPTY, ElementType.WALL],
+            [ElementType.WALL, ElementType.ROBOT, ElementType.WALL],
+        ]
+    )
+
+    assert gm.check_position(Position(1, 1)) == ElementType.EMPTY
+
+    gm.set_element(Position(1, 1), ElementType.WALL)
+
+    assert gm.check_position(Position(1, 1)) == ElementType.WALL
+
+
+def test_multiple_get_robot_position():
+    gm = RowsGameMap(
+        [
+            [ElementType.WALL, ElementType.EMPTY, ElementType.WALL],
+            [ElementType.WALL, ElementType.EMPTY, ElementType.WALL],
+            [ElementType.WALL, ElementType.ROBOT, ElementType.WALL],
+        ]
+    )
+
+    robot_pos = gm.get_robot_position()
+
+    assert robot_pos == Position(2, 1)
